08ac0b76c2ff6711e9c6deb55d70caac146f6020
Tony David Sprotte <tony.sprotte@gmx.de>
2026-02-24 18:04:22 +0100

    chore(deps): update vulnerable dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), eslintConfigPrettier];
-
-export default eslintConfig;
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  eslintConfigPrettier,
+);
